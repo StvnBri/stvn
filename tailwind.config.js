03c76038e49ad1f2b250f27b1b78154ba66301ab
STVN <83339584+StvnBri@users.noreply.github.com>
2025-03-10 21:09:40 +0800

    Adding my porfolio

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  darkMode: 'class',
+  content: ['./**/*.html'],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#3B82F6',
+        'primary-dark': '#60A5FA',
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
